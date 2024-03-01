3080a43b1e31697d4bfa1e71103c9edc58df64f5
MischkaPCHI <michellejung4002@gmail.com>
2024-03-01 14:23:03 +0100

    first commit

---
+++ homework/index.js
@@ -0,0 +1,17 @@
+const getGithubData = (p) => {
+    return fetch(p);
+}
+
+
+const dataTodo = getGithubData('https://jsonplaceholder.typicode.com/todos');
+
+dataTodo
+    .then( (response) => {return response.json()})
+    .then((result) => {
+        const containerForRepo = document.querySelector('#info');
+        result.forEach((elem) => {
+            const repElem = document.createElement('li');
+            repElem.textContent = 'UserId: ' + elem.userId + ' ID: ' + elem.id + ' Title: ' + elem.title + ' | Completed: ' + elem.completed;
+            containerForRepo.appendChild(repElem);
+        });
+    })
