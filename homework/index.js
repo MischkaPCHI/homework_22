13564604375cc8c4268604d69b691b8292431c74
Michelle <153286968+MischkaPCHI@users.noreply.github.com>
2024-03-01 14:37:20 +0100

    Update index.js

--- homework/index.js
+++ homework/index.js
@@ -15,3 +15,22 @@ dataTodo
             containerForRepo.append(repElem);
         });
     })
+
+
+// можно и так
+// const getGithubData = (p) => {
+//     return fetch(p);
+// }
+
+// const dataTodo = getGithubData('https://jsonplaceholder.typicode.com/todos');
+
+// dataTodo
+//     .then( (response) => {return response.json()})
+//     .then((result) => {result.forEach((elem) => showList(elem))});
+
+// const showList = (todo) => {
+//     const containerForRepo = document.querySelector('#info');
+//     const repElem = document.createElement('li');
+//     repElem.textContent = 'UserId: ' + todo.userId + ' ID: ' + todo.id + ' Title: ' + todo.title + ' | Completed: ' + todo.completed;
+//     containerForRepo.appendChild(repElem);
+// };
